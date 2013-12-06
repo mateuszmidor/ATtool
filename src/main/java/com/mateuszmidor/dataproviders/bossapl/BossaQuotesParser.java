d5f2b60cd7025a06b9b59b624c6ae9a51d8ab32a
mateusz <kontakt@mateuszmidor.com>
2013-12-06 19:51:09 +0100

    Added BossaQuotesParser.java Added BossaQuotesParserTest.java
    Signed-off-by: mateusz <kontakt@mateuszmidor.com>

---
+++ src/main/java/com/mateuszmidor/dataproviders/bossapl/BossaQuotesParser.java
@@ -0,0 +1,128 @@
+package com.mateuszmidor.dataproviders.bossapl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.google.common.base.Charsets;
+import com.mateuszmidor.dataproviders.DataProviderException;
+import com.mateuszmidor.dataproviders.Quotes;
+import com.mateuszmidor.dataproviders.QuotesEntry;
+
+/**
+ * Quotes parser for bossa.pl text file format
+ * 
+ * @author Mateusz Midor
+ * 
+ */
+public class BossaQuotesParser {
+
+	private static final String BOSSA_DATE_FORMAT = "yyyyMMdd";
+
+	private BossaQuotesParser() {
+	}
+
+	public static Quotes parse(String symbol, String fullName, InputStream is)
+			throws DataProviderException {
+
+		// prepare string reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(is,
+				Charsets.UTF_8));
+
+		// skip the file header
+		skipHeader(br);
+
+		// prepare quotes buffer
+		Quotes data = new Quotes(symbol, fullName);
+
+		// parse entries and insert into buffer
+		for (QuotesEntry e = getEntry(br); e != QuotesEntry.EMPTY_ENTRY; e = getEntry(br)) {
+			data.insertEntry(e);
+		}
+
+		return data;
+	}
+
+	private static QuotesEntry getEntry(BufferedReader br)
+			throws DataProviderException {
+
+		// single entry is formatted as a single line
+		// fetch one line
+		String line;
+		try {
+			line = br.readLine();
+		} catch (IOException e) {
+			throw new DataProviderException(
+					"Error during fetching single quotes entry from bossa.pl qutes data file",
+					e);
+		}
+
+		// end of entries in the stream?
+		if (null == line) {
+			return QuotesEntry.EMPTY_ENTRY;
+		}
+
+		// parse entry from the string
+		return parseEntry(line);
+	}
+
+	private static void skipHeader(BufferedReader br)
+			throws DataProviderException {
+
+		// skip one line header in quotes data stream
+		try {
+			br.readLine();
+		} catch (IOException e) {
+			throw new DataProviderException(
+					"Error during skipping header in bossa.pl quotes data file",
+					e);
+		}
+	}
+
+	private static QuotesEntry parseEntry(String s)
+			throws DataProviderException {
+		String[] fields = s.split(",");
+
+		// ticker (fields[0]) is not used
+		String date = fields[1];
+		String open = fields[2];
+		String high = fields[3];
+		String low = fields[4];
+		String close = fields[5];
+		String volume = fields[6];
+
+		QuotesEntry entry = new QuotesEntry();
+		try {
+			entry.date = parseBossaPlDate(date);
+			entry.open = parseBossaPlFloat(open);
+			entry.high = parseBossaPlFloat(high);
+			entry.low = parseBossaPlFloat(low);
+			entry.close = parseBossaPlFloat(close);
+			entry.volume = parseBossaPlFloat(volume);
+		} catch (DataProviderException | NumberFormatException e) {
+			throw new DataProviderException(
+					"Error during parsing bossa.pl quotes entry: " + s, e);
+		}
+
+		return entry;
+	}
+	private static Date parseBossaPlDate(String bossaDate)
+			throws DataProviderException {
+
+		try {
+			return new SimpleDateFormat(BOSSA_DATE_FORMAT).parse(bossaDate);
+		} catch (ParseException e) {
+			throw new DataProviderException(
+					"Error during parsing bossa.pl date: " + bossaDate, e);
+		}
+	}
+
+	private static float parseBossaPlFloat(String bossaFloat) {
+		return Float.parseFloat(bossaFloat);
+	}
+
+}
---
+++ src/test/java/com/mateuszmidor/dataproviders/bossapl/BossaQuotesParserTest.java
@@ -0,0 +1,94 @@
+package com.mateuszmidor.dataproviders.bossapl;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.mateuszmidor.dataproviders.DataProviderException;
+import com.mateuszmidor.dataproviders.Quotes;
+import com.mateuszmidor.dataproviders.QuotesEntry;
+
+public class BossaQuotesParserTest {
+
+	// for comparing floating points - open, high, low, close
+	private static final double DELTA = 0.001;
+
+	// well formatted quotes data
+	private static final String KGHM_VALID_QUOTES = "<TICKER>,<DTYYYYMMDD>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>"
+			+ '\n'
+			+ "KGHM,19970101,10.00,20.00,5.00,15.00,100"
+			+ '\n'
+			+ "KGHM,19970102,100.00,200.00,50.00,150.00,200";
+
+	// string instead of volume
+	private static final String KGHM_INVALID_VOLUME_QUOTES = "<TICKER>,<DTYYYYMMDD>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>"
+			+ '\n'
+			+ "KGHM,19970101,10.00,20.00,5.00,15.00,ahundred"
+			+ '\n'
+			+ "KGHM,19970102,100.00,200.00,50.00,150.00,twohundreds";
+
+	// invalid date format
+	private static final String KGHM_VALID_DATE_QUOTES = "<TICKER>,<DTYYYYMMDD>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>"
+			+ '\n'
+			+ "KGHM,1997-JAN-01,10.00,20.00,5.00,15.00,100"
+			+ '\n'
+			+ "KGHM,1997-JAN-02,100.00,200.00,50.00,150.00,200";
+
+	private static final InputStream KGHM_VALID_QUOTES_STREAM = new ByteArrayInputStream(
+			KGHM_VALID_QUOTES.getBytes());
+
+	private static final InputStream KGHM_INVALID_QUOTES_STREAM = new ByteArrayInputStream(
+			KGHM_INVALID_VOLUME_QUOTES.getBytes());
+
+	private static final InputStream KGHM_INVALID_DATE_STREAM = new ByteArrayInputStream(
+			KGHM_VALID_DATE_QUOTES.getBytes());
+
+	// Creates Date objects in a handy manner
+	private Date createDate(int year, int month, int day) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(year, month, day, 0, 0, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar.getTime();
+	}
+
+	@Test
+	public void testParseValidQuotes() throws DataProviderException {
+		Quotes quotes = BossaQuotesParser.parse("", "",
+				KGHM_VALID_QUOTES_STREAM);
+
+		QuotesEntry e;
+		Iterator<QuotesEntry> it = quotes.iterator();
+
+		e = it.next();
+		Assert.assertEquals(createDate(1997, Calendar.JANUARY, 1), e.date);
+		Assert.assertEquals(10.0, e.open, DELTA);
+		Assert.assertEquals(20.0, e.high, DELTA);
+		Assert.assertEquals(5.0, e.low, DELTA);
+		Assert.assertEquals(15.0, e.close, DELTA);
+		Assert.assertEquals(100.0, e.volume, DELTA);
+
+		e = it.next();
+		Assert.assertEquals(createDate(1997, Calendar.JANUARY, 2), e.date);
+		Assert.assertEquals(100.0, e.open, DELTA);
+		Assert.assertEquals(200.0, e.high, DELTA);
+		Assert.assertEquals(50.0, e.low, DELTA);
+		Assert.assertEquals(150.0, e.close, DELTA);
+		Assert.assertEquals(200.0, e.volume, DELTA);
+	}
+
+	@Test(expected = DataProviderException.class)
+	public void testParseInvalidVolumeQuotes() throws DataProviderException {
+		BossaQuotesParser.parse("", "", KGHM_INVALID_QUOTES_STREAM);
+	}
+
+	@Test(expected = DataProviderException.class)
+	public void testParseInvalidDateQuotes() throws DataProviderException {
+		BossaQuotesParser.parse("", "", KGHM_INVALID_DATE_STREAM);
+	}
+}
